c93eac6c7aa66b626324726b0d60ee363664ab2e
irah rosete <irah.rosete@outlook.com>
2022-04-19 00:47:05 +1000

    create sign up modal

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ tabs.forEach((tab) => {
         })
     })
 })
+
+// modal
+const signUpButton = document.querySelector('#signup')
+const modalBg = document.querySelector('.modal-background')
+const modal = document.querySelector('.modal')
+
+signUpButton.addEventListener('click', () => {
+    modal.classList.add('is-active')
+})
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active')
+})
